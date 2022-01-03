f5040e294fe4594c29df5ae5020413e5848f9a77
gyalmo <shrutiya1998pandey@gmail.com>
2022-01-03 22:45:46 +0530

    Let's code

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const Homepage = () => {
   return <><h1>Gyalmo's Blog!</h1>
-  <h2>Inclusion Analysis</h2>
+  <h2>This is the about page</h2>
   <p>Whether you’re a newbie or an expert in a community, when I say community I mean you can be a part of the community or leading it. Have a think who might be excluded in accessing your community activities in their current form? and why this might be. Let’s say some examples might include: people with jobs, people with caring responsibilities such as a child or another family member, wheelchair users, women and non-binary people, mature students. This is far from a complete list, but enough to get you started.
   Now when you’ve finally figured out the ones who may feel left out of the community it’s time to handle the negative scenarios.
   </p></>
