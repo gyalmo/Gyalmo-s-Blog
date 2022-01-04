934a13359d71cae522152775a03d5395064ca648
gyalmo <shrutiya1998pandey@gmail.com>
2022-01-04 20:43:09 +0530

    Let's code

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,24 @@ const Homepage = () => {
   <h2>Inclusion Analysis</h2>
   <p>Whether you’re a newbie or an expert in a community, when I say community I mean you can be a part of the community or leading it. Have a think who might be excluded in accessing your community activities in their current form? and why this might be. Let’s say some examples might include: people with jobs, people with caring responsibilities such as a child or another family member, wheelchair users, women and non-binary people, mature students. This is far from a complete list, but enough to get you started.
   Now when you’ve finally figured out the ones who may feel left out of the community it’s time to handle the negative scenarios.
+  </p> <h3>What are some of the negative scenarios which might happen while running activities for your community which will make them less inclusive to marginalized groups?
+</h3>
+  <p>The negative scenarios which might happen while running activities can be: Marginalization is one of the manifestations of gender inequality. Women may feel excluded for the opportunities in the events or tasks and that is definitely a negative scenario that will affect our community.
+The sexual minorities do face a lot of discrimination due to the dominance of heteronomous sexual relations as the only form of normal acceptable relations within he society, and this causes another negative scenario we can count on.
+People with disabilities have had to battle against biased assumptions, harmful stereotypes and irrational fears can again cause a negative scenario for us.
+If we had a registration fee for an event and there would be people who are not able to pay for it and would stop themselves from taking part in it, that would be a negative scenario for our community.
+Also, If we begin without covering the basics so it would be difficult for the people to adapt the knowledge and they may feel left out, this can be another negative scenario for our community. 
+<h3>What steps can be taken to avoid these scenarios?</h3>
+<h4>
+Thinking about how to avoid these scenarios from the beginning is key. For each of the examples above,I’ve provided a brief explanation about how you'll avoid them happening.
+
+</h4>  To avoid these scenarios we would follow up:
+We can be more inclusive especially for women by knowing them, knowing how do they feel about the community, how they feel being a part of us, giving them a chance to speak and ask others to listen to them, make them more visible by assigning them tasks and nominating more number of women for a role in the community, create opportunities for them, giving shout-outs to them and celebrate the improvements .
+We need to make people aware that people like LGBTQAI are human rights , some things are obvious , don't’ be ignorant , don't be hateful , don't discriminate, be inclusive, avoid being too nosy and show some respect. By influencing the community we can make it inclusive.
+By making sure we include people with disabilities, speak and consult directly with them, make sure that all the events and meetings are accessible to them and offer assistance to them , allow them to ask for accommodations we can provide and for them we have to be flexible with the time and locations.
+We can organize our events as open for all and without any charges or registration fee.
+To avoid this , we will make sure people attending our events are familiar with the topics.
+If we keep these negative scenarios in our mind we can even encounter arguments, for that case the best thing would be to avoid arguments else we have to start with welcoming the disagreement, try to control the temper, listen first to the volunteer, further look for areas of agreements, be honest with them and promise to think over the opponents ideas, study them carefully and thank them for their interest and show them some respect on their opinion and not to make them realise that they are wrong. Even if we are wrong at any point of view we have to admit it quickly and empathetically.
   </p></>
 };
 export default Homepage;
