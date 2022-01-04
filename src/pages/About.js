4e6bbaad4fc3468410e5546eb4f3c154bb521daf
gyalmo <shrutiya1998pandey@gmail.com>
2022-01-05 00:15:15 +0530

    Let's code

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ const about = () => {
   <p>Whether you’re a newbie or an expert in a community, when I say community I mean you can be a part of the community or leading it. Have a think who might be excluded in accessing your community activities in their current form? and why this might be. Let’s say some examples might include: people with jobs, people with caring responsibilities such as a child or another family member, wheelchair users, women and non-binary people, mature students. This is far from a complete list, but enough to get you started.
   Now when you’ve finally figured out the ones who may feel left out of the community it’s time to handle the negative scenarios.
   </p></>
-  <Link to="index">
+  <Link to="Homepage">
   home </Link>
   </main>};
 export default about;
