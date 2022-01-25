1620cbf3aef35d70908667b441b600ad31c9a0ba
gyalmo <shrutiya1998pandey@gmail.com>
2022-01-25 20:56:38 +0530

    Let's code

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,7 @@ import { Link } from "gatsby"
   return <main>
     <><h1>Gyalmo's Blog!</h1>
   <h2>This is the about page</h2>
-  <p>Whether you’re a newbie or an expert in a community, when I say community I mean you can be a part of the community or leading it. Have a think who might be excluded in accessing your community activities in their current form? and why this might be. Let’s say some examples might include: people with jobs, people with caring responsibilities such as a child or another family member, wheelchair users, women and non-binary people, mature students. This is far from a complete list, but enough to get you started.
-  Now when you’ve finally figured out the ones who may feel left out of the community it’s time to handle the negative scenarios.
+  <p> 
   </p></>
   <Link to="/">
   home </Link>
